478dc613e4d18abe8bf9be58bd0db23f8cf47c66
tuigunbekjekshenov <tuigunbekjekshenov@gmail.com>
2022-01-18 21:11:26 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hm1kt1/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.hm1kt1
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.hm1kt1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var resultLauncher : ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        registerForActivity()
+        onClickButton()
+
+
+    }
+
+
+
+    private fun onClickButton(){
+        binding.editText.setText(intent.getStringExtra(EXTRA_MESSAGE))
+        binding.clickBtn.setOnClickListener() {
+            if (binding.editText.text.toString().isEmpty()){
+                Toast.makeText(this, "jgbdfsjkgbdkfj", Toast.LENGTH_SHORT).show()
+            }else {
+                openActivityForResult()
+            }
+        }
+    }
+
+    private fun registerForActivity() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK){
+                    binding.editText.setText(result.data?.getStringExtra(TEXT))
+                }}
+    }
+
+    private fun openActivityForResult(){
+        Intent(this, MainActivity2::class.java).apply {
+            putExtra(TEXT, binding.editText.text.toString())
+            resultLauncher.launch(this)
+        }
+    }
+
+    companion object{
+        const val TEXT = "text"
+    }
+
+}
---
+++ app/src/main/java/com/example/hm1kt1/MainActivity2.kt
@@ -0,0 +1,54 @@
+package com.example.hm1kt1
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.hm1kt1.MainActivity.Companion.TEXT
+import com.example.hm1kt1.databinding.ActivityMain2Binding
+
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var viewBinding : ActivityMain2Binding
+    private lateinit var resultLauncher : ActivityResultLauncher<Intent>
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        registerForActivity()
+        onClick()
+    }
+
+    fun onClick(){
+        viewBinding.editText.setText(intent.getStringExtra(TEXT))
+        viewBinding.clickBtn.setOnClickListener(){
+            if (viewBinding.editText.text.isEmpty()){
+                Toast.makeText(this, "EditText не может быть пустым", Toast.LENGTH_SHORT).show()
+            }else openActivityForResult()
+        }
+    }
+
+    private fun registerForActivity() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK){
+                    viewBinding.editText.setText(result.data?.getStringExtra(TEXT))
+                }}
+    }
+    fun openActivityForResult(){
+        Intent(this, MainActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, viewBinding.editText.text.toString())
+            resultLauncher.launch(this)
+        }
+
+    }
+}
+
+
